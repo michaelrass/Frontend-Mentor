aa1903437e7abf3d76538684e74769d862838c9b
your_username <michaelras_@live.com>
2022-11-13 14:21:54 +0100

    javascript begun. questions and answers brought in

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-// script.js
+const buttonQuestions = document.querySelectorAll('.text-container__question');
+const answers = document.querySelectorAll('.text-container__answer');
+
+let currentIndex = 0;
+
+console.log(buttonQuestions);
+console.log(answers);
