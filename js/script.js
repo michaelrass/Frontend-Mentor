33c8368a0c41e56b1110bd7257397fd33e4be046
your_username <michaelras_@live.com>
2022-11-13 12:54:00 +0100

    javascript added

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const adviceIdWindow = document.querySelector('.number');
+const adviceTextWindow = document.querySelector('.advice-generator__paragraph');
+const buttonGetNewAdvice = document.querySelector('.advice-generator__button-generate');
+const advice_api_url = 'https://api.adviceslip.com/advice';
+
+buttonGetNewAdvice.addEventListener('click', handleButtonGetNewAdviceClick);
+
+function handleButtonGetNewAdviceClick() {
+	getNewAdvice();
+}
+
+async function getNewAdvice() {
+	const response = await fetch(advice_api_url);
+	const result = await response.json();
+	
+	const adviceId = result.slip.id;
+	const adviceText = result.slip.advice;
+
+	adviceIdWindow.innerHTML = adviceId;
+	adviceTextWindow.innerHTML = adviceText;
+}
+
+getNewAdvice();
