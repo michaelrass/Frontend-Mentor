746037c2e547aa1135100f02b5383ed9f83341b5
your_username <michaelras_@live.com>
2022-11-14 12:04:30 +0100

    empty commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 async function getData() {
-	const response = await fetch('./js/data.json');
+	const response = await fetch('https://raw.githubusercontent.com/michaelrass/Frontend-Mentor/feature/time-tracking-dashboard/js/data.json?token=GHSAT0AAAAAABYGR4FGTRXOWHZOUYBH2KXEY3SD3FQ')
 	const data = await response.json();
-
-	console.log(data)
+	console.log(data);
 }
 
 getData();
