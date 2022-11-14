a7813afc6596181c8ce6aa25bfb69ef518b00349
your_username <michaelras_@live.com>
2022-11-14 12:33:49 +0100

    fetched data from github raw, added all the tabs in index

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+const tabContainers = document.querySelectorAll('.time-tracking__tab-container');
+
 async function getData() {
-	const response = await fetch('https://raw.githubusercontent.com/michaelrass/Frontend-Mentor/feature/time-tracking-dashboard/js/data.json?token=GHSAT0AAAAAABYGR4FGTRXOWHZOUYBH2KXEY3SD3FQ')
+	const response = await fetch('https://raw.githubusercontent.com/michaelrass/Frontend-Mentor/feature/time-tracking-dashboard/js/data.json?token=GHSAT0AAAAAABYGR4FGRYPGXVWBPOGIFMHGY3SENRA')
 	const data = await response.json();
+
 	console.log(data);
 }
-
-getData();
