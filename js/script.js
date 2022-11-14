ed1cbdd7f37f4556aebefe26c3202442a2ecbcf8
your_username <michaelras_@live.com>
2022-11-14 14:06:53 +0100

    changed background color of tabs to correct ones. Struggling on javascript

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const tabContainers = document.querySelectorAll('.time-tracking__tab-container');
+const tabTitles = document.querySelectorAll('.title__title');
+const tabTimesCurrent = document.querySelectorAll('.time__current');
+const tabTimesHistory = document.querySelectorAll('.time__history');
+
+
+
+console.log(tabTitles, tabTimesCurrent, tabTimesHistory);
+let currentIndex = 0;
 
 async function getData() {
-	const response = await fetch('https://raw.githubusercontent.com/michaelrass/Frontend-Mentor/feature/time-tracking-dashboard/js/data.json?token=GHSAT0AAAAAABYGR4FGRYPGXVWBPOGIFMHGY3SENRA')
+	const response = await fetch('https://raw.githubusercontent.com/michaelrass/Frontend-Mentor/feature/time-tracking-dashboard/js/data.json')
 	const data = await response.json();
 
-	console.log(data);
+	
 }
+
+getData();
