b82a0034e1c38de57cf1d533878731513b238abb
your_username <michaelras_@live.com>
2022-11-15 08:57:39 +0100

    buttons, questions and answers works perfectly, and classlist add/remove is good. cleaned tree, structured functions.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const buttonQuestions = document.querySelectorAll('.text-container__question');
 const answers = document.querySelectorAll('.text-container__answer');
 const picturesArrow = document.querySelectorAll('.arrow-down');
 
+
+buttonQuestions.forEach(button => {
+	button.classList.remove('text-container__question--active');
+});
+
+picturesArrow.forEach(picture => {
+	picture.classList.remove('arrow-down--flipped');
+});
+
+answers.forEach(answer => {
+	answer.classList.remove('text-container__answer--visible');
+});		
+
+
 let currentIndex = 0;
 
 for (let index = 0; index < buttonQuestions.length; index += 1) {
@@ -15,12 +29,8 @@ function handleButtonQuestionsClick(event, index) {
 }
 
 function openNextQuestion(event, index) {
-	render(index);
-}
-
-function render(index) {
-	
-	currentIndex = index;
+	if (!answers[index].classList.contains('text-container__answer--visible')) {
+		currentIndex = index;
 	buttonQuestions.forEach(button => {
 		button.classList.remove('text-container__question--active');
 	})
@@ -31,10 +41,30 @@ function render(index) {
 	
 	answers.forEach(answer => {
 		answer.classList.remove('text-container__answer--visible');
-	})	
+	})
+
 	buttonQuestions[currentIndex].classList.add('text-container__question--active');
 	picturesArrow[currentIndex].classList.add('arrow-down--flipped');
 	answers[currentIndex].classList.add('text-container__answer--visible');
+	} else {
+	answers[currentIndex].classList.remove('text-container__answer--visible');
+	picturesArrow[currentIndex].classList.remove('arrow-down--flipped');
+	buttonQuestions[currentIndex].classList.remove('text-container__question--active');
+	}
+}
+
+function render() {
+	buttonQuestions.forEach(button => {
+		button.classList.remove('text-container__question--active');
+	});
+	
+	picturesArrow.forEach(picture => {
+		picture.classList.remove('arrow-down--flipped');
+	});
+	
+	answers.forEach(answer => {
+		answer.classList.remove('text-container__answer--visible');
+	});	
 }
 
 render();
